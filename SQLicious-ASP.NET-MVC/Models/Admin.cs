34e8c6aba35e7e07b6422be5314e22571ae55e28
manhattaa <fadynhatta@gmail.com>
2024-09-02 12:58:08 +0200

    Admin and MenuItem Models; Admin is powered by MS Identity

---
+++ SQLicious-ASP.NET-MVC/Models/Admin.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SQLicious_ASP.NET_MVC.Models
+{
+    public class Admin : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
