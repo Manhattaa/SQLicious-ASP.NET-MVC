4a500004099ec06a1a954d7360f002369461b289
manhattaa <fadynhatta@gmail.com>
2024-09-02 12:57:50 +0200

    Created a RestaurantContext that only will handle Admin + MenuItem related queries

---
+++ SQLicious-ASP.NET-MVC/Data/RestaurantContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using SQLicious_ASP.NET_MVC.Models;
+
+namespace SQLicious_ASP.NET_MVC.Data
+{
+    public class RestaurantContext : DbContext
+    {
+        public RestaurantContext(DbContextOptions<RestaurantContext> options) : base(options) { }
+        public DbSet<MenuItems> MenuItems { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+    }
+}
