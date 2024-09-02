a272a6e7bf6efdfa124dbd8a63308e49c306e76f
manhattaa <fadynhatta@gmail.com>
2024-09-02 13:36:44 +0200

    Updated the RestaurantContext to incorporate the React project as well. and implemented Migrations

--- SQLicious-ASP.NET-MVC/Data/RestaurantContext.cs
+++ SQLicious-ASP.NET-MVC/Data/RestaurantContext.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using SQLicious_ASP.NET_MVC.Models;
 
 namespace SQLicious_ASP.NET_MVC.Data
@@ -7,8 +6,11 @@ namespace SQLicious_ASP.NET_MVC.Data
     public class RestaurantContext : DbContext
     {
         public RestaurantContext(DbContextOptions<RestaurantContext> options) : base(options) { }
-        public DbSet<MenuItems> MenuItems { get; set; }
+        public DbSet<Table> Tables { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
         public DbSet<Admin> Admins { get; set; }
+        public DbSet<MenuItems> Menus { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
