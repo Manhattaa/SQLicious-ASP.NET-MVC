cf38f0330464872477adf7ec39780566d15ae3a1
manhattaa <fadynhatta@gmail.com>
2024-09-08 00:06:47 +0200

    Just added sample data to the AdminRepository

--- SQLicious-ASP.NET-MVC/Data/Repository/AdminRepository.cs
+++ SQLicious-ASP.NET-MVC/Data/Repository/AdminRepository.cs
@@ -1,10 +1,31 @@
 ﻿using SQLicious_ASP.NET_MVC.Data.Repository.IRepository;
+using SQLicious_ASP.NET_MVC.Models;
 
 namespace SQLicious_ASP.NET_MVC.Data.Repository
 {
     public class AdminRepository : IAdminRepository
     {
+        public Task<Admin> CreateAdmin(Admin admin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Admin> DeleteAdmin(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Admin> GetAdminById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
         public Task<IEnumerable<IAdminRepository>> GetAllAdmins()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Admin> UpdateAdmin(Admin admin)
         {
             throw new NotImplementedException();
         }
