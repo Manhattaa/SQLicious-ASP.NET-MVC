b835b85c112067fe819e0a3082748bc432e9c62a
manhattaa <fadynhatta@gmail.com>
2024-09-08 00:06:31 +0200

    Added a bunch of methods to my AdminRepository Interface

--- SQLicious-ASP.NET-MVC/Data/Repository/IRepository/IAdminRepository.cs
+++ SQLicious-ASP.NET-MVC/Data/Repository/IRepository/IAdminRepository.cs
@@ -1,7 +1,13 @@
-﻿namespace SQLicious_ASP.NET_MVC.Data.Repository.IRepository
+﻿using SQLicious_ASP.NET_MVC.Models;
+
+namespace SQLicious_ASP.NET_MVC.Data.Repository.IRepository
 {
     public interface IAdminRepository
     {
         Task<IEnumerable<IAdminRepository>> GetAllAdmins();
+        Task<Admin> GetAdminById(int id);
+        Task<Admin> CreateAdmin(Admin admin);
+        Task<Admin> UpdateAdmin(Admin admin);
+        Task<Admin> DeleteAdmin(int id);
     }
 }
