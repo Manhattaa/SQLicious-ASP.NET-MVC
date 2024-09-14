91bc82f122715e162a66d4cd053914fb5fdd8be0
manhattaa <fadynhatta@gmail.com>
2024-09-14 22:27:31 +0200

    Created a ConfirmEmailRequestDTO that will handle the data sent during the Confirmation phase

---
+++ SQLicious-ASP.NET-MVC/Models/DTO/ConfirmEmailRequestDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace SQLicious_ASP.NET_MVC.Models.DTO
+{
+    public class ConfirmEmailRequestDTO
+    {
+        public string UserId { get; set; }
+        public string Code { get; set; }
+    }
+}
