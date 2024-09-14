8e099b005cd7c3c32cf682bc4b16daf5b79ac35d
manhattaa <fadynhatta@gmail.com>
2024-09-14 22:26:09 +0200

    Added and qualified most of the methods for the Admin phase.

--- SQLicious-ASP.NET-MVC/Data/Repository/IRepository/IAdminRepository.cs
+++ SQLicious-ASP.NET-MVC/Data/Repository/IRepository/IAdminRepository.cs
@@ -1,15 +1,19 @@
-﻿using SQLicious_ASP.NET_MVC.Models;
+﻿using Microsoft.AspNetCore.Identity;
+using SQLicious_ASP.NET_MVC.Models;
+using SQLicious_ASP.NET_MVC.Options;
+using System.Security.Claims;
 
 namespace SQLicious_ASP.NET_MVC.Data.Repository.IRepository
 {
     public interface IAdminRepository
     {
-        Task<IEnumerable<IAdminRepository>> GetAllAdmins();
+        Task<IEnumerable<Admin>> GetAllAdmins();
         Task<Admin> GetAdminById(int id);
-        Task<Admin> CreateAdmin(Admin admin);
-        Task<Admin> UpdateAdmin(Admin admin);
-        Task<Admin> DeleteAdmin(int id);
-        Task<Admin> Login(Admin admin);
-        Task<Admin> Logout(Admin admin);
+        Task<IdentityResult> CreateAdminAsync(Admin admin, string password);
+        Task<IdentityResult> UpdateAdminAsync(Admin admin);
+        Task<IdentityResult> DeleteAdminAsync(string password, ClaimsPrincipal currentUser);
+        Task<LoginResult> LoginAsync(string email, string password);
+        Task<IdentityResult> SendEmailVerificationAsync(string userId, string code);
+        Task<Admin> GetAdminByEmailAsync(string email);
     }
 }
