910dd5be06c122f7987da0293eb4d376ccb69148
manhattaa <fadynhatta@gmail.com>
2024-09-14 22:29:26 +0200

    Created an Email Result handler as well as Sender.

---
+++ SQLicious-ASP.NET-MVC/Options/Email/EmailResult.cs
@@ -0,0 +1,8 @@
+﻿namespace SQLicious_ASP.NET_MVC.Options.Email
+{
+    public class EmailResult
+    {
+        public bool Success { get; set; }
+        public string? ErrorMessage { get; set; }
+    }
+}
---
+++ SQLicious-ASP.NET-MVC/Options/Email/IEmail/IEmailSender.cs
@@ -0,0 +1,7 @@
+﻿namespace SQLicious_ASP.NET_MVC.Options.Email.IEmail
+{
+    public interface IEmailSender
+    {
+        public Task<EmailResult> SendEmailAsync(string email, string subject, string body);
+    }
+}
