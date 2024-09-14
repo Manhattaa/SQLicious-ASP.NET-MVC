e0db5832ff0572d87209dc85f1bcc937d062b5c1
manhattaa <fadynhatta@gmail.com>
2024-09-14 22:26:36 +0200

    Removed GetThisWeeksMenu as it felt a little unnecessary

--- SQLicious-ASP.NET-MVC/Data/Repository/IRepository/IMenuItemRepository.cs
+++ SQLicious-ASP.NET-MVC/Data/Repository/IRepository/IMenuItemRepository.cs
@@ -5,7 +5,6 @@ namespace SQLicious_ASP.NET_MVC.Data.Repository.IRepository
     public interface IMenuItemRepository
     {
         Task<IEnumerable<MenuItems>> GetAllMenuItemsAsync();
-        Task<IEnumerable<MenuItems>> GetThisWeeksMenu();
         Task<MenuItems> GetMenuItemByIdAsync(int menuItemId);
         Task CreateMenuItemAsync(MenuItems menuItem);
         Task UpdateMenuItemAsync(MenuItems menuItem);
