d3f02583a18424b18aea6872a6cb4172d852bed1
manhattaa <fadynhatta@gmail.com>
2024-09-14 22:28:28 +0200

    LoginRequestDTO that will aid us with the login phase.

---
+++ SQLicious-ASP.NET-MVC/Models/DTO/LoginRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SQLicious_ASP.NET_MVC.Models.DTO
+{
+    public class LoginRequestDTO
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Enter a valid email.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; set; }
+    }
+}
