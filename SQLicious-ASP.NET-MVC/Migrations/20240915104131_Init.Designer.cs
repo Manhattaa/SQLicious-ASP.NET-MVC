f84b0666ac8e18cad243b800ba1c032bb10c96c0
manhattaa <fadynhatta@gmail.com>
2024-09-15 13:47:36 +0200

    New Migrations since DbContext previously wasnt compatible with Identity.

--- SQLicious-ASP.NET-MVC/Migrations/20240915104131_Init.Designer.cs
+++ SQLicious-ASP.NET-MVC/Migrations/20240915104131_Init.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SQLicious_ASP.NET_MVC.Data;
 
@@ -11,9 +12,11 @@ using SQLicious_ASP.NET_MVC.Data;
 namespace SQLicious_ASP.NET_MVC.Migrations
 {
     [DbContext(typeof(RestaurantContext))]
-    partial class RestaurantContextModelSnapshot : ModelSnapshot
+    [Migration("20240915104131_Init")]
+    partial class Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
