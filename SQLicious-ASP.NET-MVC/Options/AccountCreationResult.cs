b4f533f876064c3e504b4ed949c978da289aa40d
manhattaa <fadynhatta@gmail.com>
2024-09-15 13:49:25 +0200

    Created an AccountCreationResult

---
+++ SQLicious-ASP.NET-MVC/Options/AccountCreationResult.cs
@@ -0,0 +1,10 @@
+﻿namespace SQLicious_ASP.NET_MVC.Options
+{
+    public class AccountCreationResult
+    {
+        public bool Success { get; set; }
+        public string? Token { get; set; }
+        //To support multiple errorMessages
+        public IEnumerable<string>? Errors { get; set; }
+    }
+}
