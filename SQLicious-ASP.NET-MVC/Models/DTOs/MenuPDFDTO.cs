ad6ed362dcb33fbb43df3ecab1de29ef7ea27d5e
manhattaa <fadynhatta@gmail.com>
2024-09-23 21:54:09 +0200

    Created an MenuPDFDTO that will aid with the generation phase.

---
+++ SQLicious-ASP.NET-MVC/Models/DTOs/MenuPDFDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace SQLicious_ASP.NET_MVC.Models.DTOs
+{
+    public class MenuPDFDTO
+    {
+        public int MenuPDFId { get; set; } 
+        public string MenuType { get; set; }  
+        public string PdfUrl { get; set; } 
+    }
+}
