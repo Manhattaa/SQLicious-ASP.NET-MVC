7d842acc8297451c08518638f8158c26fe44a673
manhattaa <fadynhatta@gmail.com>
2024-09-24 21:52:49 +0200

    Added a helper class that will aid us with the Pagination.

---
+++ SQLicious-ASP.NET-MVC/Helpers/PagedResult.cs
@@ -0,0 +1,25 @@
+﻿namespace SQLicious_ASP.NET_MVC.Helpers
+{
+    public class PagedResult<T> where T : class
+    {
+        public IEnumerable<T> Results { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+
+        public PagedResult(IEnumerable<T> results, int currentPage, int totalPages, int pageSize)
+        {
+            Results = results;
+            CurrentPage = currentPage;
+            TotalPages = totalPages;
+            PageSize = pageSize;
+        }
+
+        public static PagedResult<T> Create(IEnumerable<T> source, int page, int pageSize)
+        {
+            var totalItems = source.Count();
+            var items = source.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+            return new PagedResult<T>(items, page, (int)Math.Ceiling(totalItems / (double)pageSize), pageSize);
+        }
+    }
+}
