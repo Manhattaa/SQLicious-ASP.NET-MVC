907379c8fb986f1cd7a7a9cc7c917e2da2a56b9e
manhattaa <fadynhatta@gmail.com>
2024-09-26 22:05:18 +0200

    Created 2FA Dtos that are made for verification and creation purposes

---
+++ SQLicious-ASP.NET-MVC/Models/DTOs/TwoFactorAuthDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace SQLicious_ASP.NET_MVC.Models.DTOs
+{
+    public class TwoFactorAuthDTO
+    {
+        public string QrCodeUrl { get; set; }
+    }
+}
---
+++ SQLicious-ASP.NET-MVC/Models/DTOs/Verify2FADTO.cs
@@ -0,0 +1,7 @@
+﻿namespace SQLicious_ASP.NET_MVC.Models.DTOs
+{
+    public class Verify2FADTO
+    {
+        public string Code { get; set; }
+    }
+}
